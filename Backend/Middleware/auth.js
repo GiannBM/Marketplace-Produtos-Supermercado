bfd7228a8008505507ffe4fae0ccedb3b025907e
GiannBM <115822719+GiannBM@users.noreply.github.com>
2025-06-09 00:02:32 -0300

    .

---
+++ Backend/Middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+
+const SECRET = "4gcRTEpUllFv3AlJ76Af4evdkNtiow5n";
+
+
+function JWTverroute(req, res, next){
+
+    const token = req.headers['x-acess-token']
+    
+    try{
+
+        const decoded = jwt.verify(token, SECRET)
+        req.id = decoded.id
+        req.email = decoded.email
+        next();
+
+    }catch(err){
+        res.status(401).send(err)
+    }
+
+}
+
+
+module.exports = JWTverroute;
